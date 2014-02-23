da1d5ab892e70872608719fa53b04892a9c6c02e
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2014-02-22 21:19:27 -0600

    libdfp: Format test-decode indentation

--- tests/test-decode.c
+++ tests/test-decode.c
@@ -1,6 +1,6 @@
 /* Test decoded facility.
 
-   Copyright (C) 2009-2012 Free Software Foundation, Inc.
+   Copyright (C) 2009-2014 Free Software Foundation, Inc.
 
    This file is part of the Decimal Floating Point C Library.
 
@@ -28,38 +28,28 @@
 
 #include <float.h>
 #include <stdio.h>
-#include <wchar.h> /* This should pick up the libdfp wchar in dfp/wchar.h.  */
+#include <wchar.h>		/* This should pick up the libdfp wchar in dfp/wchar.h.  */
 
 #define _WANT_DC 1
-#include "scaffold.c" /* Pick up the _DC_P(x,y,...) macro.  */
+#include "scaffold.c"		/* Pick up the _DC_P(x,y,...) macro.  */
 
 /* Inspired by GLIBC stdio-common/tfformat.c  */
-typedef struct{
+typedef struct
+{
   int line;
   _Decimal128 d;
   const char *expect;
 } d128_type;
 
-typedef struct{
+typedef struct
+{
   int line;
   _Decimal64 d;
   const char *expect;
 } d64_type;
 
-/*typedef union{
-  _Decimal128 d;
-  long i[4];
-} d128_u;
-
-typedef union{
-  _Decimal64 d;
-  long i[4];
-} d64_u; */
-
-#define DEC_INFINITY	__builtin_infd32()
-#define DEC_NAN		(0.0DF * DEC_INFINITY)
-
-int main (void)
+int
+main (void)
 {
   d64_type *d64ptr = NULL;
   d128_type *d128ptr = NULL;
@@ -70,52 +60,41 @@ int main (void)
   d128_u d128u;
   d128u.d = 1.00DL; */
 
-  d64_type d64types[] =
-    {
-#ifdef _DPD_BACKEND
-      {__LINE__,1.00DD, "+0,000,000,000,000,100E-2"},
-      {__LINE__,__DEC64_MIN__, "+0,000,000,000,000,001E-383"},
-      {__LINE__,__DEC64_MAX__, "+9,999,999,999,999,999E+369"},
-      {__LINE__,-DEC_NAN, "-0,000,000,000,000,000E-398"},
-      {__LINE__,9.999999999999999E369DD, "+9,999,999,999,999,999E+354"},
-      {__LINE__,9.999999999999999E370DD, "+9,999,999,999,999,999E+355"},
-      {__LINE__,9.999999999999999E384DD, "+9,999,999,999,999,999E+369"},
-      {__LINE__,__DEC64_MIN__, "+0,000,000,000,000,001E-383"},
-      {__LINE__,__DEC64_SUBNORMAL_MIN__, "+0,000,000,000,000,001E-398"},
-      {__LINE__,1E-398DD, "+0,000,000,000,000,001E-398" },
-      /* Notice, this number exceeds DEC64_SUBNORMAL_MIN.  */
-      {__LINE__,1E-399DD, "+0,000,000,000,000,000E-398" },
-      {__LINE__, 9E370DD, "+0,000,000,000,000,090E+369"},
-      {__LINE__, 4E384DD, "+4,000,000,000,000,000E+369"},
-#else
-      {__LINE__,1.00DD, "BID not supported."},
-#endif
-      {0,0,0 }
-    };
+  d64_type d64types[] = {
+    {__LINE__, 1.00DD, "+0,000,000,000,000,100E-2"},
+    {__LINE__, __DEC64_MIN__, "+0,000,000,000,000,001E-383"},
+    {__LINE__, __DEC64_MAX__, "+9,999,999,999,999,999E+369"},
+    {__LINE__, -__builtin_nand64(""), "-0,000,000,000,000,000E-398"},
+    {__LINE__, 9.999999999999999E369DD, "+9,999,999,999,999,999E+354"},
+    {__LINE__, 9.999999999999999E370DD, "+9,999,999,999,999,999E+355"},
+    {__LINE__, 9.999999999999999E384DD, "+9,999,999,999,999,999E+369"},
+    {__LINE__, __DEC64_MIN__, "+0,000,000,000,000,001E-383"},
+    {__LINE__, __DEC64_SUBNORMAL_MIN__, "+0,000,000,000,000,001E-398"},
+    {__LINE__, 1E-398DD, "+0,000,000,000,000,001E-398"},
+    {__LINE__, 9E370DD, "+0,000,000,000,000,090E+369"},
+    {__LINE__, 4E384DD, "+4,000,000,000,000,000E+369"},
+    {0, 0, 0}
+  };
 
-  d128_type d128types[] =
-    {
-#ifdef _DPD_BACKEND
-      {__LINE__,1.00DL, "+0,000,000,000,000,000,000,000,000,000,000,100E-2"},
-#else
-      {__LINE__,1.00DL, "BID not supported."},
-#endif
-      /* log10d128(0.0000000011DL)  */
-      {__LINE__, -8.958607314841774959249800028756976DL, "-8,958,607,314,841,774,959,249,800,028,756,976E-33"},
-      {  0,0,0 }
-    };
+  d128_type d128types[] = {
+    {__LINE__, 1.00DL, "+0,000,000,000,000,000,000,000,000,000,000,100E-2"},
+    /* log10d128(0.0000000011DL)  */
+    {__LINE__, -8.958607314841774959249800028756976DL,
+     "-8,958,607,314,841,774,959,249,800,028,756,976E-33"},
+    {0, 0, 0}
+  };
 
   for (d128ptr = d128types; d128ptr->line; d128ptr++)
     {
-      _DC_P(__FILE__,d128ptr->line, d128ptr->expect,d128ptr->d);
+      _DC_P (__FILE__, d128ptr->line, d128ptr->expect, d128ptr->d);
     }
 
   for (d64ptr = d64types; d64ptr->line; d64ptr++)
     {
-      _DC_P(__FILE__,d64ptr->line, d64ptr->expect,d64ptr->d);
+      _DC_P (__FILE__, d64ptr->line, d64ptr->expect, d64ptr->d);
     }
 
-  _REPORT();
+  _REPORT ();
 
   /* fail comes from scaffold.c  */
   return fail;
