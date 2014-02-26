687360dcbf7fa6f59b289ae7629ec4a3a7ea7687
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2014-02-26 09:38:27 -0600

    libdfp: Name change for test-round.c
    This patch changes the name of test-round.c to test-fenv.c (showing
    it is indeed testing rounding related to fenv) and add some minor
    cleaups in the code.

--- tests/test-fenv.c
+++ tests/test-fenv.c
@@ -1,6 +1,6 @@
 /* Test fe_dec_getround() and fe_dec_setround()
 
-   Copyright (C) 2010 Free Software Foundation, Inc.
+   Copyright (C) 2010-2014 Free Software Foundation, Inc.
 
    This file is part of the Decimal Floating Point C Library.
 
@@ -26,51 +26,48 @@
 #define __STDC_WANT_DEC_FP__
 #endif
 
-#include <float.h> /* DEC_NAN definition.  */
+#include <float.h>		/* DEC_NAN definition.  */
 #include <stdio.h>
 #include <math.h>
 #include <fenv.h>
 
-#define _WANT_VC 1 /* Pick up the _VC_P(x,y,fmt) macro.  */
-#include "scaffold.c" /* Pick up the _VC_P(x,y,fmt) macro.  */
+#define _WANT_VC 1		/* Pick up the _VC_P(x,y,fmt) macro.  */
+#include "scaffold.c"		/* Pick up the _VC_P(x,y,fmt) macro.  */
 
-/* Inspired by GLIBC stdio-common/tfformat.c  */
-typedef struct{
+typedef struct
+{
   int line;
-  int set;     /* Set it to this.  */
-  int expect;  /* Result should be this.  */
-  const char *format; /* printf %d */
+  int set;			/* Set it to this.  */
+  int expect;			/* Result should be this.  */
 } d_type;
 
-d_type printf_rms[] =
-{
-  {__LINE__, FE_DEC_TONEAREST, FE_DEC_TONEAREST,  "%d"},
-  {__LINE__, FE_DEC_TOWARDZERO, FE_DEC_TOWARDZERO,  "%d"},
-  {__LINE__, FE_DEC_UPWARD, FE_DEC_UPWARD,  "%d"},
-  {__LINE__, FE_DEC_DOWNWARD, FE_DEC_DOWNWARD,  "%d"},
-  {__LINE__, FE_DEC_TONEARESTFROMZERO, FE_DEC_TONEARESTFROMZERO,  "%d"},
-  {__LINE__, 5, 5, "%d"}, /* Non-spec hardware rounding mode.  */
-  {__LINE__, 6, 6, "%d"}, /* Non-spec hardware rounding mode.  */
-  {__LINE__, 7, 7, "%d"}, /* Non-spec hardware rounding mode.  */
-  {0,0,0,0 }
+static const d_type round_env[] = {
+  {__LINE__, FE_DEC_TONEAREST, FE_DEC_TONEAREST},
+  {__LINE__, FE_DEC_TOWARDZERO, FE_DEC_TOWARDZERO},
+  {__LINE__, FE_DEC_UPWARD, FE_DEC_UPWARD},
+  {__LINE__, FE_DEC_DOWNWARD, FE_DEC_DOWNWARD},
+  {__LINE__, FE_DEC_TONEARESTFROMZERO, FE_DEC_TONEARESTFROMZERO},
+  {__LINE__, 5, 5},		/* Non-spec hardware rounding mode.  */
+  {__LINE__, 6, 6},		/* Non-spec hardware rounding mode.  */
+  {__LINE__, 7, 7},		/* Non-spec hardware rounding mode.  */
 };
 
-int main (void)
-{
-  d_type *dptr;
+static const int round_env_size = sizeof (round_env) / sizeof (round_env[0]);
 
-  for (dptr = printf_rms; dptr->line; dptr++)
+int
+main (void)
+{
+  int i;
+  for (i = 0; i < round_env_size; ++i)
     {
       int retval;
-      fe_dec_setround(dptr->set);
-      retval = fe_dec_getround();
-      _VC_P(__FILE__,dptr->line, dptr->expect,retval,dptr->format);
+      fe_dec_setround (round_env[i].set);
+      retval = fe_dec_getround ();
+      _VC_P (__FILE__, round_env[i].line, round_env[i].expect, retval, "%d");
     }
 
-  _REPORT();
+  _REPORT ();
 
   /* fail comes from scaffold.c  */
   return fail;
 }
-
-
