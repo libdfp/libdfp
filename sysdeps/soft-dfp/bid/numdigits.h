e1c321eb21a58efe3074cdf62fe528541c6d9dbc
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2014-05-18 17:24:44 -0300

    libdfp: Implement sysdeps/soft-dfp/bid/numdigits.h
    This patch implements the numdigits functions for BID. They are:
    
    - getexpd[32|64|128]: returns the _Decimal expoenent value as int.
    - setexpd[32|64|128]: sets the _Decimal expoenent from an int value.
    - numdigitsd[32|64|128]: return the number of digits from coefficient.
    - setdigitsd[32|64|128]: sets the coefficient value.
    - left_justifyd[32|64|128]: left justify the decimal value.

--- sysdeps/soft-dfp/bid/numdigits.h
+++ sysdeps/soft-dfp/bid/numdigits.h
@@ -20,60 +20,417 @@
 
    Please see dfp/COPYING.txt for more information.  */
 
-/* Allow this file to be included more than once, so don't use the usual
-   include guards.  */
-
 #define NUMDIGITS_SUPPORT 1
 
-#ifndef DEC_TYPE
-#error DEC_TYPE must be declared
-#endif
+/* NOTE: Little-Endian support only so far.  */
 
-#ifndef _DECIMAL_SIZE
-#error _DECIMAL_SIZE must be declared
-#endif
+#ifndef _BID_NUMDIGITS_H
+# define _BID_NUMDIGITS_H
 
-#ifndef PASTE
-# define PASTE(x,y) PASTE2(x,y)
-# define PASTE2(x,y) x##y
-#endif
+#include <string.h>
+#include <stdlib.h>
 
-#ifndef FUNC_D
-# define FUNC_D(x)		PASTE(x,PASTE(d,_DECIMAL_SIZE))
-#endif
+#include <bid-private.h>
+#include <get_digits.h>
 
-static inline int
-FUNC_D (getexp) (DEC_TYPE x)
-{
-  // TODO
-  return 0;
-}
-#define SETEXP_NAME	PASTE(setexp,PASTE(d,_DECIMAL_SIZE))
-
-static inline DEC_TYPE
-SETEXP_NAME (DEC_TYPE x, int exp)
-{
-  // TODO
-  return x;
-}
-
-static inline DEC_TYPE
-FUNC_D (setdigits) (DEC_TYPE x, char *str)
-{
-  // TODO
-  return x;
-}
+#define BID_SIG_MASK            0x80000000u
+#define BID_EXPONENT_ENC_MASK   0x60000000u
+#define BID_EXP_SHIFT_LARGE32   21
+#define BID_EXP_SHIFT_SMALL32   23
+#define BID_EXP_MASK32          0xffu
+#define BID_EXP_SHIFT_LARGE64   19
+#define BID_EXP_SHIFT_SMALL64   21
+#define BID_EXP_MASK64          0x3ffu
+#define BID_EXP_SHIFT_LARGE128  15
+#define BID_EXP_SHIFT_SMALL128  17
+#define BID_EXP_MASK128         0x3fffu
 
 static inline int
-FUNC_D (numdigits) (DEC_TYPE x)
+getexpd32 (_Decimal32 x)
 {
-  // TODO
-  return 0;
+  unsigned int hi;
+  unsigned int shift;
+  union ieee754r_Decimal32 d;
+
+  d.sd = x;
+  hi = d.si;
+
+  if ((hi & BID_EXPONENT_ENC_MASK) == BID_EXPONENT_ENC_MASK)
+    shift = BID_EXP_SHIFT_LARGE32;
+  else
+    shift = BID_EXP_SHIFT_SMALL32;
+
+  return ((hi >> shift) & BID_EXP_MASK32) - DECIMAL32_Bias;
 }
 
-static inline DEC_TYPE
-FUNC_D (left_justify) (DEC_TYPE x)
+static inline int
+getexpd64 (_Decimal64 x)
 {
-  // TODO
+  unsigned int hi;
+  unsigned int shift;
+  union ieee754r_Decimal64 d;
+
+  d.dd = x;
+  hi = d.di[1];
+
+  if ((hi & BID_EXPONENT_ENC_MASK) == BID_EXPONENT_ENC_MASK)
+    shift = BID_EXP_SHIFT_LARGE64;
+  else
+    shift = BID_EXP_SHIFT_SMALL64;
+
+  return ((hi >> shift) & BID_EXP_MASK64) - DECIMAL64_Bias;
+}
+
+static inline int
+getexpd128 (_Decimal128 x)
+{
+  unsigned int hi;
+  unsigned int shift;
+  union ieee754r_Decimal128 d;
+
+  d.td = x;
+  hi = d.ti[3];
+
+  if ((hi & BID_EXPONENT_ENC_MASK) == BID_EXPONENT_ENC_MASK)
+    shift = BID_EXP_SHIFT_LARGE128;
+  else
+    shift = BID_EXP_SHIFT_SMALL128;
+
+  return (int)((hi >> shift) & BID_EXP_MASK128) - DECIMAL128_Bias;
+}
+
+
+static inline _Decimal32
+setexpd32 (_Decimal32 x, int exp)
+{
+  unsigned int shift;
+  union ieee754r_Decimal32 d;
+
+  d.sd = x;
+
+  if ((d.si & BID_EXPONENT_ENC_MASK) == BID_EXPONENT_ENC_MASK)
+    shift = BID_EXP_SHIFT_LARGE32;
+  else
+    shift = BID_EXP_SHIFT_SMALL32;
+
+  d.si &= ~((BID_EXP_MASK32) << shift);
+  d.si |= ((exp + DECIMAL32_Bias) & BID_EXP_MASK32) << shift;
+  return d.sd;
+}
+
+static inline _Decimal64
+setexpd64 (_Decimal64 x, int exp)
+{
+  unsigned int shift;
+  union ieee754r_Decimal64 d;
+
+  d.dd = x;
+
+  if ((d.di[1] & BID_EXPONENT_ENC_MASK) == BID_EXPONENT_ENC_MASK)
+    shift = BID_EXP_SHIFT_LARGE64;
+  else
+    shift = BID_EXP_SHIFT_SMALL64;
+
+  d.di[1] &= ~((BID_EXP_MASK64) << shift);
+  d.di[1] |= ((exp + DECIMAL64_Bias) & BID_EXP_MASK64) << shift;
+  return d.dd;
+}
+
+static inline _Decimal128
+setexpd128 (_Decimal128 x, int exp)
+{
+  unsigned int shift;
+  union ieee754r_Decimal128 d;
+
+  d.td = x;
+
+  if ((d.ti[3] & BID_EXPONENT_ENC_MASK) == BID_EXPONENT_ENC_MASK)
+    shift = BID_EXP_SHIFT_LARGE128;
+  else
+    shift = BID_EXP_SHIFT_SMALL128;
+
+  d.ti[3] &= ~((BID_EXP_MASK128) << shift);
+  d.ti[3] |= ((exp + DECIMAL128_Bias) & BID_EXP_MASK128) << shift;
+  return d.td;
+}
+
+
+static inline int
+numdigitsd32 (_Decimal32 x)
+{
+  int firstdigit = 0;
+  int len = 0;
+  char digits[8] = { 0 };
+
+  __get_digits_d32 (x, digits, NULL, NULL, NULL, NULL);
+
+  while (digits[firstdigit] == '0')
+    firstdigit++;
+
+  len = strlen (digits + firstdigit);
+  /* Hardware DFP always returns 1 digit if the mantissa is zero.  We should
+   * do the same.  */
+  return (len == 0 ? 1 : len);
+}
+
+static inline int
+numdigitsd64 (_Decimal64 x)
+{
+  int firstdigit = 0;
+  int len = 0;
+  char digits[17] = { 0 };
+
+  __get_digits_d64 (x, digits, NULL, NULL, NULL, NULL);
+
+  while (digits[firstdigit] == '0')
+    firstdigit++;
+
+  len = strlen (digits + firstdigit);
+  /* Hardware DFP always returns 1 digit if the mantissa is zero.  We should
+   * do the same.  */
+  return (len == 0 ? 1 : len);
+}
+
+static inline int
+numdigitsd128 (_Decimal128 x)
+{
+  int firstdigit = 0;
+  int len = 0;
+  char digits[34] = { 0 };
+
+  __get_digits_d128 (x, digits, NULL, NULL, NULL, NULL);
+
+  while (digits[firstdigit] == '0')
+    firstdigit++;
+
+  len = strlen (digits + firstdigit);
+  /* Hardware DFP always returns 1 digit if the mantissa is zero.  We should
+   * do the same.  */
+  return (len == 0 ? 1 : len);
+}
+
+static inline
+int __bid_required_bits_32 (unsigned long int value)
+{
+  return value == 0 ? 0 : (32 - __builtin_clz (value));
+}
+
+static inline _Decimal32
+setdigitsd32 (_Decimal32 x, char *str)
+{
+  unsigned long int number;
+  union ieee754r_Decimal32 d;
+  unsigned int sign;
+  int exp;
+
+  d.sd = x;
+  number = strtol (str, NULL, 10);
+
+  exp = getexpd32 (x);
+  sign = (d.si & BID_SIG_MASK);
+  if (__bid_required_bits_32 (number) <= 23)
+    d.si = sign | (number & 0x807FFFFFU);
+  else
+    d.si = sign | (0x60800000U | (number & 0x001FFFFFU));
+  d.sd = setexpd32 (d.sd, exp);
+
+  return d.sd;
+}
+
+
+static inline
+int __bid_required_bits_64 (unsigned long long int value)
+{
+  return (value == 0ULL ? 0 : 64 - __builtin_clzll (value));
+}
+
+static inline _Decimal64
+setdigitsd64 (_Decimal64 x, char *str)
+{
+  unsigned long long int number;
+  union ieee754r_Decimal64 d;
+  unsigned int sign;
+  int exp;
+
+  d.dd = x;
+  number = strtoll (str, NULL, 10);
+
+  exp = getexpd64 (x);
+  sign = (d.di[1] & BID_SIG_MASK);
+  if (__bid_required_bits_64 (number) <= 53)
+    d.di[1] = sign | ((number >> 32L) & 0x001FFFFFU);
+  else
+    d.di[1] = (sign | (0x60040000U) | ((number >> 32L) & 0x0007FFFFU));
+  d.di[0] = number & 0xFFFFFFFFU;
+  d.dd = setexpd64 (d.dd, exp);
+
+  return d.dd;
+}
+
+static inline __uint128_t
+get_number128 (char* sz)
+{
+  // make some reasonable assumptions
+  int radix = 10, size, i;
+  char ch;
+  __uint128_t a;
+  unsigned int n;
+  int minus = 0;
+
+  size = strlen (sz);
+
+  a = 0;
+  i = 0;
+
+  // Check for minus sign.
+  if (sz[i] == '-')
+    {
+      ++i;
+      minus = 1;
+    }
+
+  while (i < size)
+    {
+      ch = sz[i];
+
+      if (ch >= 'A' && ch <= 'Z')
+	{
+	  if (((ch - 'A') + 10) < radix)
+	    n = (ch - 'A') + 10;
+	  else
+	    break;
+	}
+      else if (ch >= 'a' && ch <= 'z')
+        {
+	  if (((ch - 'a') + 10) < radix)
+	    n = (ch - 'a') + 10;
+	  else
+	    break;
+	}
+      else if (ch >= '0' && ch <= '9')
+	{
+	  if ((ch - '0') < radix)
+	    n = (ch - '0');
+	  else
+	    break;
+	}
+      else
+	/* Completely invalid character  */
+	break;
+
+      a *= radix;
+      a += n;
+      ++i;
+    }
+
+  /* If this was a negative number, do that two's compliment  */
+  if (minus)
+    a = -a;
+
+  return a;
+}
+
+static inline
+int __bid_required_bits_128 (__uint128_t value)
+{
+  /* Use 64-bits operations to keep it simple, another solution would
+   * be to construct a lookup table for int128.  */
+  int ret;
+  unsigned long long int hi = (value >> 64);
+  if ((ret = __bid_required_bits_64 (hi)) == 0)
+    {
+      unsigned long long int lo = (value & 0xFFFFFFFFFFFFFFFFULL);
+      return __bid_required_bits_64 (lo);
+    }
+  return ret + 64;
+}
+
+static inline _Decimal128
+setdigitsd128 (_Decimal128 x, char *str)
+{
+  __uint128_t number;
+  union ieee754r_Decimal128 d;
+  unsigned int sign;
+  int exp;
+
+  d.td = x;
+  number = get_number128 (str);
+
+  exp = getexpd128 (x);
+  sign = (d.ti[3] & BID_SIG_MASK);
+  if (__bid_required_bits_128 (number) <= 113)
+    d.ti[3] = sign | ((number >> 96) & 0x0001FFFF);
+  else
+    d.ti[3] = (sign | (0x60004000U) | ((number >> 96) & 0x00007FFFU));
+  d.ti[2] = (number >> 64) & 0xFFFFFFFFU;
+  d.ti[1] = (number >> 32) & 0xFFFFFFFFU;
+  d.ti[0] = number & 0xFFFFFFFFU;
+  d.td = setexpd128 (d.td, exp);
+
+  return d.td;
+}
+
+static inline _Decimal32
+left_justifyd32 (_Decimal32 x)
+{
+  int firstdigit = 0, len;
+  char digits[8+7] = { 0 };
+
+  __get_digits_d32 (x, digits, NULL, NULL, NULL, NULL);
+  while (digits[firstdigit] == '0')
+    firstdigit++;
+  len = strlen (digits + firstdigit);
+  if (len)
+    {
+      /* pad the significant digits with enough trailing zeroes */
+      memset(digits + firstdigit + len, '0', firstdigit);
+      x = setdigitsd32 (x, digits + firstdigit);
+      x = setexpd32 (x, getexpd32 (x) - firstdigit);
+    }
+
   return x;
 }
+
+static inline _Decimal64
+left_justifyd64 (_Decimal64 x)
+{
+  int firstdigit = 0, len;
+  char digits[16+17] = { 0 };
+
+  __get_digits_d64 (x, digits, NULL, NULL, NULL, NULL);
+  while (digits[firstdigit] == '0')
+    firstdigit++;
+  len = strlen (digits + firstdigit);
+  if (len)
+    {
+      /* pad the significant digits with enough trailing zeroes */
+      memset(digits + firstdigit + len, '0', firstdigit);
+      x = setdigitsd64 (x, digits + firstdigit);
+      x = setexpd64 (x, getexpd64 (x) - firstdigit);
+    }
+
+  return x;
+}
+
+static inline _Decimal128
+left_justifyd128 (_Decimal128 x)
+{
+  int firstdigit = 0, len;
+  char digits[35+34] = { 0 };
+
+  __get_digits_d128 (x, digits, NULL, NULL, NULL, NULL);
+  while (digits[firstdigit] == '0')
+    firstdigit++;
+  len = strlen (digits + firstdigit);
+  if (len)
+    {
+      /* pad the significant digits with enough trailing zeroes */
+      memset(digits + firstdigit + len, '0', firstdigit);
+      x = setdigitsd128 (x, digits + firstdigit);
+      x = setexpd128 (x, getexpd128 (x) - firstdigit);
+    }
+
+  return x;
+}
+
+#endif
