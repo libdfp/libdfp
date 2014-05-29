996bcf3eb552a09b50f4f47deff967000c5f08b7
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2014-05-29 09:14:50 -0500

    libdfp: Add weak_alias assembler definition

--- include/libdfp-symbols.h
+++ include/libdfp-symbols.h
@@ -47,9 +47,15 @@
 
 /* Define ALIASNAME as a weak alias for NAME.
    If weak aliases are not available, this defines a strong alias.  */
-#define weak_alias(name, aliasname) _weak_alias (name, aliasname)
-#define _weak_alias(name, aliasname) \
+#ifndef __ASSEMBLER__
+# define weak_alias(name, aliasname) _weak_alias (name, aliasname)
+# define _weak_alias(name, aliasname) \
   extern __typeof (name) aliasname __attribute__ ((weak, alias (#name)));
+#else
+# define weak_alias(original, alias) \
+  .weak alias; \
+  alias = original
+#endif
 
 /* Same as WEAK_ALIAS, but mark symbol as hidden.  */
 #define weak_hidden_alias(name, aliasname) \
