4241e7c944dbd5171ea371e81824eeda1ea9d442
Paul E. Murphy <murphyp@linux.vnet.ibm.com>
2015-07-09 12:42:50 -0500

    Add ChangeLog generator script
    Ideally, this would run as a result of 'make dist.'
    
    Most of the older commits follow an inconsistent style,
    so the dynamic generation stops at this point, and appends
    a markdown friendly version of the pruned changelog.
    
    Signed-off-by: Paul E. Murphy <murphyp@linux.vnet.ibm.com>

---
+++ generate-changelog.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Generate markdown ChangeLog files based on git history.
+
+tmp=$(mktemp)
+touch ${tmp}
+
+# Commits prior to this one contain mangled commit history.
+# Reformatting these requires some extra handholding
+LASTCOMMIT="2d48e88ad67f600376926f073302b2617e49675c"
+
+prev_tag="$LASTCOMMIT"
+echo "# ChangeLog"
+for tag in $(git tag -l [0-9]* | sort -V); do
+	echo "handle $tag ... $prev_tag"
+	echo "${prev_tag:+${prev_tag}..}${tag}"
+	# Prepend tags to the output because we want the ChangeLog in the
+	# reverse order, e.g. 2.0.0, 1.0.1 and 1.0.0.
+	{
+		date=$(git show --date=short --format='tformat:%cd' -s ${tag} | tail -n 1)
+		echo -e "\n## ${tag} - ${date}\n"
+		git --no-pager log --no-merges --format='format: - %s%n' \
+		    ${prev_tag:+${prev_tag}..}${tag} | cat - ${tmp}
+	} > ${tmp}.1
+	mv ${tmp}.1 ${tmp}
+	prev_tag=${tag}
+done
+
+# Send the output to stdout.
+cat ${tmp}
+
+# Append a revised changelog for stuff prior to LASTCOMMIT in
+# consistent style with the generated comments
+cat ChangeLog.old.md
+
+rm ${tmp}
