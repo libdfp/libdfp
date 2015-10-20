209e17412d5b3e7f34cfcef3e1a7a33f11b0b431
Tulio Magno Quites Machado Filho <tuliom@linux.vnet.ibm.com>
2015-10-20 10:32:20 -0200

    Minor fixes to make-release.sh
    Fix indentation and punctuation and remove trailing white space.
    
    Signed-off-by: Tulio Magno Quites Machado Filho <tuliom@linux.vnet.ibm.com>

--- make-release.sh
+++ make-release.sh
@@ -2,35 +2,34 @@
 
 logfile=${PWD}/release.$(date +%y-%m-%d-%T).log
 
-#
 # This script attempts to generate a release like tarball
 # of the project in the absense of full autotooling support
 #
 
 # Since the autotooling support is missing for now
-# we'll create an archive with git-archive
-
+# we'll create an archive with git-archive.
 if [ ! -d .git ]; then
 	echo "Fatal: Not a git project. Bailing"
 	exit 1
 fi
 
-# Determine the library name and version
+# Determine the library name and version.
 PROJVER=libdfp-$(git describe --tags)
 
-# Generate the archive
+# Generate the archive.
 if ! git archive --format=tar --prefix=${PROJVER}/ HEAD > ${PROJVER}.tar ; then
 	echo "Failed to archive"
 	exit 1
 fi
 
-# Generate a changelog and append it
+# Generate a changelog and append it.
 mkdir ${PROJVER}
 ./generate-changelog.sh > ${PROJVER}/ChangeLog.md
 tar rf ${PROJVER}.tar ${PROJVER}/ChangeLog.md
 
-# Remove any files which needn't exist 
-tar f ${PROJVER}.tar --delete ${PROJVER}/{make-release.sh,generate-changelog.sh,ChangeLog.old.md}
+# Remove any files which needn't exist.
+tar f ${PROJVER}.tar \
+    --delete ${PROJVER}/{make-release.sh,generate-changelog.sh,ChangeLog.old.md}
 rm -rf ${PROJVER}
 gzip -f ${PROJVER}.tar
 
@@ -38,10 +37,10 @@ tarballs="${PROJVER}.tar.gz"
 
 
 tar xf ${PROJVER}.tar.gz --warning=no-timestamp
-# Attempt a VPATH build
+# Attempt a VPATH build.
 cd ${PROJVER}
 
-# Update timestamps to prevent odd issues
+# Update timestamps to prevent odd issues.
 touch configure.ac aclocal.m4 configure Makefile.am Makefile.in
 
 mkdir _build
@@ -79,12 +78,12 @@ rm -rf libdfp
 
 echo "Found tarballs: $tarballs"
 
-# Generate some popular checksums for the tarball
+# Generate some popular checksums for the tarball.
 for tarball in ${tarballs}; do
-        for sum in sha256 md5; do
-                echo "Generating ${tarball}.${sum}"
-                ${sum}sum ${tarball} > ${tarball}.${sum}
-        done
+	for sum in sha256 md5; do
+		echo "Generating ${tarball}.${sum}"
+		${sum}sum ${tarball} > ${tarball}.${sum}
+	done
 done
 
 
