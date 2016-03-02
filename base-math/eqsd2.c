5c0064ab0a88299234be8903a653b3197734a871
Stefan Liebler <stli@linux.vnet.ibm.com>
2016-03-02 12:42:49 +0100

    libdfp: Add hidden definition for base_math symbols.
    This patch makes the libdfp _Decimal operations hava a hidden definition.
    See patch "libdfp: base-math symbol hidden_def" (2014-05-19).
    
    This patch adds the hidden_def for all functions in base-math folder,
    which have hidden_proto_enc in dfpacc.h.
    
    On S390, these base-math/* files are used, which leads to a different
    generation of <build-dir>/libdfp.symlist, than on power.
    With this patch, they are identical.
    
    Signed-off-by: Stefan Liebler <stli@linux.vnet.ibm.com>

--- base-math/eqsd2.c
+++ base-math/eqsd2.c
@@ -1,6 +1,6 @@
 /* Decimal Floating Point function intended to override the libgcc version.
 
-   Copyright (C) 2009-2015 Free Software Foundation, Inc.
+   Copyright (C) 2009-2016 Free Software Foundation, Inc.
 
    This file is part of the Decimal Floating Point C Library.
 
@@ -29,3 +29,4 @@ __BACKEND_(eqsd2) (_Decimal32 x, _Decimal32 y)
 {
   return (_Decimal64)x == (_Decimal64)y ? 0 : 1;
 }
+hidden_def (__BACKEND_(eqsd2))
--- base-math/floatunssisd.c
+++ base-math/floatunssisd.c
@@ -1,6 +1,6 @@
 /* Decimal Floating Point function intended to override the libgcc version.
 
-   Copyright (C) 2009-2015 Free Software Foundation, Inc.
+   Copyright (C) 2009-2016 Free Software Foundation, Inc.
 
    This file is part of the Decimal Floating Point C Library.
 
@@ -29,3 +29,4 @@ __BACKEND_(floatunssisd) (unsigned int x)
 {
   return (_Decimal32)x;
 }
+hidden_def (__BACKEND_(floatunssisd))
