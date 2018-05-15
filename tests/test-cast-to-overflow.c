a0e1d24cb45394ba3a2da4cbf23f3d6001617790
Stefan Liebler <stli@linux.vnet.ibm.com>
2018-05-15 13:28:15 +0200

    Add more under/overflow tests for dfp to bfp conversion.
    It seems as the tests use incorrect input values. See more information in issue #71.
    According to the comment from Joseph Myers in "Conversions incorrect on underflow #32",
    "This fix looks wrong for FE_TONEAREST mode, when conversion of a value with absolute
    value less than or equal to half the least subnormal in the target type should
    produce +/- 0, not +/- the least subnormal."
    
    The following min-values are defined for the bfp types on s390x:
    define __FLT_DENORM_MIN__ 1.40129846432481707092372958328991613e-45F
    define __DBL_DENORM_MIN__ ((double)4.94065645841246544176568792868221372e-324L)
    define __LDBL_DENORM_MIN__ 6.47517511943802511092443895822764655e-4966L
    
    => half of the least subnormal (__DBL_DENORM_MIN__) = 2.4703282292062327e-324.
    And not 2.4...26e-325.
    => half of the least subnormal (__FLT_DENORM_MIN__) = 7.0064923e-46
    And not 7.0...22e-46.
    
    This commit adds some more values for under / overflow tests.
    and it adds tests for the remaining conversions:
    -dfp128 -> bfp128
    -dfp64 -> bfp64
    -dfp64 -> bfp32
    -dfp32 -> bfp32
    
    Note: On s390x (with hardware-dfp), all those tests are passing if the
    pfpo instruction is rounding according the BFP-rounding-mode.
    Currently GCC is emitting the pfpo instruction with rounding according
    the DFP-rounding-mode.
    
    Signed-off-by: Stefan Liebler <stli@linux.vnet.ibm.com>

--- tests/test-cast-to-overflow.c
+++ tests/test-cast-to-overflow.c
@@ -39,47 +39,201 @@ typedef struct
   int line;
   int round_mode;
   _Decimal128 set;
+  long double expect;
+} d128_type;
+
+typedef struct
+{
+  int line;
+  int round_mode;
+  _Decimal64 set;
   double expect;
-} d_type;
+} d64_type;
+
+typedef struct
+{
+  int line;
+  int round_mode;
+  _Decimal32 set;
+  float expect;
+} d32_type;
+
+/* Decimal 128 to Binary Double 128.  */
+static const d128_type d128to128[] = {
+ {__LINE__, FE_TONEAREST, 1e6000DL, INFINITY},
+ {__LINE__, FE_TONEAREST, -1e6000DL, -INFINITY},
+ {__LINE__, FE_TONEAREST, 1e4933DL, INFINITY},
+ {__LINE__, FE_TONEAREST, -1e4933DL, -INFINITY},
+#if __LDBL_MANT_DIG__ == 106
+ {__LINE__, FE_TONEAREST, 1.797693134862316e308DL, INFINITY},
+ {__LINE__, FE_TONEAREST, -1.797693134862316e308DL, -INFINITY},
+#else
+ {__LINE__, FE_TONEAREST, 1.189731495357232e4932DL, INFINITY},
+ {__LINE__, FE_TONEAREST, -1.189731495357232e4932DL, -INFINITY},
+#endif
+ {__LINE__, FE_TOWARDZERO, 1e6000DL, LDBL_MAX},
+ {__LINE__, FE_TOWARDZERO, -1e6000DL, -LDBL_MAX},
+ {__LINE__, FE_TOWARDZERO, 1e4933DL, LDBL_MAX},
+ {__LINE__, FE_TOWARDZERO, -1e4933DL, -LDBL_MAX},
+#if __LDBL_MANT_DIG__ == 106
+ {__LINE__, FE_TOWARDZERO, 1.797693134862316e308DL, LDBL_MAX},
+ {__LINE__, FE_TOWARDZERO, -1.797693134862316e308DL, -LDBL_MAX},
+#else
+ {__LINE__, FE_TOWARDZERO, 1.189731495357232e4932DL, LDBL_MAX},
+ {__LINE__, FE_TOWARDZERO, -1.189731495357232e4932DL, -LDBL_MAX},
+#endif
+ {__LINE__, FE_DOWNWARD, 1e6000DL, LDBL_MAX},
+ {__LINE__, FE_DOWNWARD, -1e6000DL, -INFINITY},
+ {__LINE__, FE_DOWNWARD, 1e4933DL, LDBL_MAX},
+ {__LINE__, FE_DOWNWARD, -1e4933DL, -INFINITY},
+#if __LDBL_MANT_DIG__ == 106
+ {__LINE__, FE_DOWNWARD, 1.797693134862316e308DL, LDBL_MAX},
+ {__LINE__, FE_DOWNWARD, -1.797693134862316e308DL, -INFINITY},
+#else
+ {__LINE__, FE_DOWNWARD, 1.189731495357232e4932DL, LDBL_MAX},
+ {__LINE__, FE_DOWNWARD, -1.189731495357232e4932DL, -INFINITY},
+#endif
+ {__LINE__, FE_UPWARD, 1e6000DL, INFINITY},
+ {__LINE__, FE_UPWARD, -1e6000DL, -LDBL_MAX},
+ {__LINE__, FE_UPWARD, 1e4933DL, INFINITY},
+ {__LINE__, FE_UPWARD, -1e4933DL, -LDBL_MAX},
+#if __LDBL_MANT_DIG__ == 106
+ {__LINE__, FE_UPWARD, 1.797693134862316e308DL, INFINITY},
+ {__LINE__, FE_UPWARD, -1.797693134862316e308DL, -LDBL_MAX}
+#else
+ {__LINE__, FE_UPWARD, 1.189731495357232e4932DL, INFINITY},
+ {__LINE__, FE_UPWARD, -1.189731495357232e4932DL, -LDBL_MAX}
+#endif
+};
 
 /* Decimal 128 to Binary Double 64.  */
-static const d_type d128to64[] = {
+static const d128_type d128to64[] = {
  {__LINE__, FE_TONEAREST, 1e1000DL, INFINITY},
  {__LINE__, FE_TONEAREST, -1e1000DL, -INFINITY},
+ {__LINE__, FE_TONEAREST, 1.797693134862316e308DL, INFINITY},
+ {__LINE__, FE_TONEAREST, -1.797693134862316e308DL, -INFINITY},
  {__LINE__, FE_TOWARDZERO, 1e1000DL, DBL_MAX},
  {__LINE__, FE_TOWARDZERO, -1e1000DL, -DBL_MAX},
+ {__LINE__, FE_TOWARDZERO, 1.797693134862316e308DL, DBL_MAX},
+ {__LINE__, FE_TOWARDZERO, -1.797693134862316e308DL, -DBL_MAX},
  {__LINE__, FE_DOWNWARD, 1e1000DL, DBL_MAX},
  {__LINE__, FE_DOWNWARD, -1e1000DL, -INFINITY},
+ {__LINE__, FE_DOWNWARD, 1.797693134862316e308DL, DBL_MAX},
+ {__LINE__, FE_DOWNWARD, -1.797693134862316e308DL, -INFINITY},
  {__LINE__, FE_UPWARD, 1e1000DL, INFINITY},
- {__LINE__, FE_UPWARD, -1e1000DL, -DBL_MAX}
+ {__LINE__, FE_UPWARD, -1e1000DL, -DBL_MAX},
+ {__LINE__, FE_UPWARD, 1.797693134862316e308DL, INFINITY},
+ {__LINE__, FE_UPWARD, -1.797693134862316e308DL, -DBL_MAX}
 };
 
 /* Decimal 128 to Binary Float 32.  */
-static const d_type d128to32[] = {
+static const d128_type d128to32[] = {
  {__LINE__, FE_TONEAREST, 1e1000DL, INFINITY},
  {__LINE__, FE_TONEAREST, -1e1000DL, -INFINITY},
+ {__LINE__, FE_TONEAREST, 3.4028236e38DL, INFINITY},
+ {__LINE__, FE_TONEAREST, -3.4028236e38DL, -INFINITY},
  {__LINE__, FE_TOWARDZERO, 1e1000DL, FLT_MAX},
  {__LINE__, FE_TOWARDZERO, -1e1000DL, -FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, 3.4028236e38DL, FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, -3.4028236e38DL, -FLT_MAX},
  {__LINE__, FE_DOWNWARD, 1e1000DL, FLT_MAX},
  {__LINE__, FE_DOWNWARD, -1e1000DL, -INFINITY},
+ {__LINE__, FE_DOWNWARD, 3.4028236e38DL, FLT_MAX},
+ {__LINE__, FE_DOWNWARD, -3.4028236e38DL, -INFINITY},
  {__LINE__, FE_UPWARD, 1e1000DL, INFINITY},
- {__LINE__, FE_UPWARD, -1e1000DL, -FLT_MAX}
+ {__LINE__, FE_UPWARD, -1e1000DL, -FLT_MAX},
+ {__LINE__, FE_UPWARD, 3.4028236e38DL, INFINITY},
+ {__LINE__, FE_UPWARD, -3.4028236e38DL, -FLT_MAX}
 };
 
-static const int d128to64_size = sizeof (d128to64) / sizeof (d128to64[0]);
+/* Decimal 64 to Binary Double 64.  */
+static const d64_type d64to64[] = {
+ {__LINE__, FE_TONEAREST, 1e369DD, INFINITY},
+ {__LINE__, FE_TONEAREST, -1e369DD, -INFINITY},
+ {__LINE__, FE_TONEAREST, 1.797693134862316e308DD, INFINITY},
+ {__LINE__, FE_TONEAREST, -1.797693134862316e308DD, -INFINITY},
+ {__LINE__, FE_TOWARDZERO, 1e369DD, DBL_MAX},
+ {__LINE__, FE_TOWARDZERO, -1e369DD, -DBL_MAX},
+ {__LINE__, FE_TOWARDZERO, 1.797693134862316e308DD, DBL_MAX},
+ {__LINE__, FE_TOWARDZERO, -1.797693134862316e308DD, -DBL_MAX},
+ {__LINE__, FE_DOWNWARD, 1e369DD, DBL_MAX},
+ {__LINE__, FE_DOWNWARD, -1e369DD, -INFINITY},
+ {__LINE__, FE_DOWNWARD, 1.797693134862316e308DD, DBL_MAX},
+ {__LINE__, FE_DOWNWARD, -1.797693134862316e308DD, -INFINITY},
+ {__LINE__, FE_UPWARD, 1e369DD, INFINITY},
+ {__LINE__, FE_UPWARD, -1e369DD, -DBL_MAX},
+ {__LINE__, FE_UPWARD, 1.797693134862316e308DD, INFINITY},
+ {__LINE__, FE_UPWARD, -1.797693134862316e308DD, -DBL_MAX}
+};
 
+/* Decimal 64 to Binary Float 32.  */
+static const d64_type d64to32[] = {
+ {__LINE__, FE_TONEAREST, 1e369DD, INFINITY},
+ {__LINE__, FE_TONEAREST, -1e369DD, -INFINITY},
+ {__LINE__, FE_TONEAREST, 3.402823567797337e38DD, INFINITY},
+ {__LINE__, FE_TONEAREST, -3.402823567797337e38DD, -INFINITY},
+ {__LINE__, FE_TOWARDZERO, 1e369DD, FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, -1e369DD, -FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, 3.402823567797337e38DD, FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, -3.402823567797337e38DD, -FLT_MAX},
+ {__LINE__, FE_DOWNWARD, 1e369DD, FLT_MAX},
+ {__LINE__, FE_DOWNWARD, -1e369DD, -INFINITY},
+ {__LINE__, FE_DOWNWARD, 3.402823567797337e38DD, FLT_MAX},
+ {__LINE__, FE_DOWNWARD, -3.402823567797337e38DD, -INFINITY},
+ {__LINE__, FE_UPWARD, 1e369DD, INFINITY},
+ {__LINE__, FE_UPWARD, -1e369DD, -FLT_MAX},
+ {__LINE__, FE_UPWARD, 3.402823567797337e38DD, INFINITY},
+ {__LINE__, FE_UPWARD, -3.402823567797337e38DD, -FLT_MAX}
+};
+
+/* Decimal 64 to Binary Float 32.  */
+static const d32_type d32to32[] = {
+ {__LINE__, FE_TONEAREST, 1e90DF, INFINITY},
+ {__LINE__, FE_TONEAREST, -1e90DF, -INFINITY},
+ {__LINE__, FE_TONEAREST, 3.402826e38DF, INFINITY},
+ {__LINE__, FE_TONEAREST, -3.402826e38DF, -INFINITY},
+ {__LINE__, FE_TOWARDZERO, 1e90DF, FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, -1e90DF, -FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, 3.402826e38DF, FLT_MAX},
+ {__LINE__, FE_TOWARDZERO, -3.402826e38DF, -FLT_MAX},
+ {__LINE__, FE_DOWNWARD, 1e90DF, FLT_MAX},
+ {__LINE__, FE_DOWNWARD, -1e90DF, -INFINITY},
+ {__LINE__, FE_DOWNWARD, 3.402826e38DF, FLT_MAX},
+ {__LINE__, FE_DOWNWARD, -3.402826e38DF, -INFINITY},
+ {__LINE__, FE_UPWARD, 1e90DF, INFINITY},
+ {__LINE__, FE_UPWARD, -1e90DF, -FLT_MAX},
+ {__LINE__, FE_UPWARD, 3.402826e38DF, INFINITY},
+ {__LINE__, FE_UPWARD, -3.402826e38DF, -FLT_MAX}
+};
+
+static const int d128to128_size = sizeof (d128to128) / sizeof (d128to128[0]);
+static const int d128to64_size = sizeof (d128to64) / sizeof (d128to64[0]);
 static const int d128to32_size = sizeof (d128to32) / sizeof (d128to32[0]);
+
+static const int d64to64_size = sizeof (d64to64) / sizeof (d64to64[0]);
+static const int d64to32_size = sizeof (d64to32) / sizeof (d64to32[0]);
+
+static const int d32to32_size = sizeof (d32to32) / sizeof (d32to32[0]);
 
 int
 main(void)
 {
   int i;
+  for (i=0; i<d128to128_size; ++i)
+    {
+      long double ret;
+      fesetround (d128to128[i].round_mode);
+      ret = (long double) d128to128[i].set;
+      _VC_P (__FILE__, d128to128[i].line, d128to128[i].expect, ret, "%La");
+    }
+
   for (i=0; i<d128to64_size; ++i)
     {
       double ret;
       fesetround (d128to64[i].round_mode);
       ret = (double) d128to64[i].set;
-      _VC_P (__FILE__, d128to64[i].line, d128to64[i].expect, ret, "%a");
+      _VC_P (__FILE__, d128to64[i].line, d128to64[i].expect, (long double) ret,
+	     "%La");
     }
 
   for (i=0; i<d128to32_size; ++i)
@@ -87,7 +241,32 @@ main(void)
       float ret;
       fesetround (d128to32[i].round_mode);
       ret = (float) d128to32[i].set;
-      _VC_P (__FILE__, d128to32[i].line, d128to32[i].expect, ret, "%a");
+      _VC_P (__FILE__, d128to32[i].line, d128to32[i].expect, (long double) ret,
+	     "%La");
+    }
+
+  for (i=0; i<d64to64_size; ++i)
+    {
+      double ret;
+      fesetround (d64to64[i].round_mode);
+      ret = (double) d64to64[i].set;
+      _VC_P (__FILE__, d64to64[i].line, d64to64[i].expect, ret, "%a");
+    }
+
+  for (i=0; i<d64to32_size; ++i)
+    {
+      float ret;
+      fesetround (d64to32[i].round_mode);
+      ret = (float) d64to32[i].set;
+      _VC_P (__FILE__, d64to32[i].line, d64to32[i].expect, ret, "%a");
+    }
+
+  for (i=0; i<d32to32_size; ++i)
+    {
+      float ret;
+      fesetround (d32to32[i].round_mode);
+      ret = (float) d32to32[i].set;
+      _VC_P (__FILE__, d32to32[i].line, d32to32[i].expect, ret, "%a");
     }
 
   _REPORT();
