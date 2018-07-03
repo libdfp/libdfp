3bf12e752eaf5678f1a83be419ddbaf5d2df9961
Stefan Liebler <stli@linux.ibm.com>
2018-07-03 12:58:31 +0200

    S390: Add hardware support for signbitd32|64|128 functions.
    Hardware instructions are used for signbit instead of ieee754r-version
    on s390 with hard-dfp.
    
    Signed-off-by: Stefan Liebler <stli@linux.ibm.com>

---
+++ sysdeps/s390/dfpu/signbitd32.c
@@ -0,0 +1,54 @@
+/* Returns non-zero if _Decimal32 is negative
+
+   Copyright (C) 2018 Free Software Foundation, Inc.
+
+   The Decimal Floating Point C Library is free software; you can
+   redistribute it and/or modify it under the terms of the GNU Lesser
+   General Public License version 2.1.
+
+   The Decimal Floating Point C Library is distributed in the hope that
+   it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+   the GNU Lesser General Public License version 2.1 for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License version 2.1 along with the Decimal Floating Point C Library;
+   if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+   Suite 330, Boston, MA 02111-1307 USA.
+
+   Please see libdfp/COPYING.txt for more information.  */
+
+#ifndef _DECIMAL_SIZE
+# define _DECIMAL_SIZE 32
+#endif
+
+#include <math.h>
+#include <ieee754r_private.h>
+
+#ifndef FUNCTION_NAME
+# define FUNCTION_NAME signbit
+#endif
+#include <dfpmacro.h>
+
+int
+INTERNAL_FUNCTION_NAME (DEC_TYPE x)
+{
+  int result;
+  __asm__ ("lhi %0,1      \n\t" /* Is negative.  */
+	   /* Is -zero, -subnormal, -inf, -nan?  */
+#if _DECIMAL_SIZE == 128
+	   "tdcxt %1,1365 \n\t"
+#elif _DECIMAL_SIZE == 64
+	   "tdcdt %1,1365 \n\t"
+#else
+	   "tdcet %1,1365 \n\t"
+#endif
+	   "jl 1f         \n\t"  /* Jump away if it is negative.  */
+	   "lhi %0,0      \n\t"  /* Is positive.  */
+	   "1:"
+	   : "=d" (result) : "f" (x) : "cc");
+
+  return result;
+}
+hidden_def (INTERNAL_FUNCTION_NAME)
+weak_alias (INTERNAL_FUNCTION_NAME, EXTERNAL_FUNCTION_NAME)
