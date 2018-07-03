4a1daec0c72876e021f144843d4b2feab3388104
Stefan Liebler <stli@linux.ibm.com>
2018-07-03 12:58:31 +0200

    S390: Add hardware support for roundd32|64|128 functions.
    Hardware instructions are used for round instead of ieee754r-version
    on s390 with hard-dfp.
    
    Signed-off-by: Stefan Liebler <stli@linux.ibm.com>

---
+++ sysdeps/s390/dfpu/roundd32.c
@@ -0,0 +1,52 @@
+/* Rounds to the nearest integer value, ignores rounding mode
+
+   Copyright (C) 2018 Free Software Foundation, Inc.
+
+   This file is part of the Decimal Floating Point C Library.
+
+   The Decimal Floating Point C Library is free software; you can
+   redistribute it and/or modify it under the terms of the GNU Lesser
+   General Public License version 2.1.
+
+   The Decimal Floating Point C Library is distributed in the hope that
+   it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+   the GNU Lesser General Public License version 2.1 for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License version 2.1 along with the Decimal Floating Point C Library;
+   if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+   Suite 330, Boston, MA 02111-1307 USA.
+
+   Please see libdfp/COPYING.txt for more information.  */
+
+#ifndef _DECIMAL_SIZE
+# define _DECIMAL_SIZE 32
+#endif
+
+#include <math.h>
+
+#define FUNCTION_NAME round
+#include <dfpmacro.h>
+
+DEC_TYPE
+INTERNAL_FUNCTION_NAME (DEC_TYPE x)
+{
+#if _DECIMAL_SIZE == 128
+  _Decimal128 tmp = x;
+#else
+  _Decimal64 tmp = (_Decimal64) x;
+#endif
+
+  /* Round toward to nearest with ties away from 0 without inexact
+     exception.  */
+  __asm__ (
+#if _DECIMAL_SIZE == 128
+	   "fixtr %0,12,%0,4"
+#else
+	   "fidtr %0,12,%0,4"
+#endif
+	   : "+f" (tmp));
+  return (DEC_TYPE) tmp;
+}
+weak_alias (INTERNAL_FUNCTION_NAME, EXTERNAL_FUNCTION_NAME)
