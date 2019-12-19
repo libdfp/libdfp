bd08dd8954f9e408c0b4e5d26def5b45ac7d137b
Paul E. Murphy <murp@ibm.com>
2019-12-19 09:39:22 -0600

    powd64: cleanup with 'indent'
    This was copied without change from the powerpc sysdep dir.
    
    Likewise, remove the POWER6/powerpc comment.  This is
    implemented in pure _Decimal{64,128}.
    
    It could use some cleaning, so use the 'indent' tool to
    format this per GNU standards.

--- ieee754r/powd64.c
+++ ieee754r/powd64.c
@@ -1,4 +1,4 @@
-/* Decimal Floating Point 64-bit x^y.  POWER6 PowerPC64 version.
+/* Decimal Floating Point 64-bit x^y.
 
    Copyright (C) 2008-2015 Free Software Foundation, Inc.
 
@@ -27,15 +27,15 @@
 #include <ieee754r_private.h>
 
 #ifndef _DECIMAL_SIZE
-#  define _DECIMAL_SIZE 64
-#  include <decimal64.h>
+#define _DECIMAL_SIZE 64
+#include <decimal64.h>
 #endif
 
 #define FUNCTION_NAME pow
 
 #include <dfpmacro.h>
 
-static DEC_TYPE 
+static DEC_TYPE
 intpow (DEC_TYPE val, int N)
 {
   DEC_TYPE result = 1;
@@ -44,7 +44,8 @@ intpow (DEC_TYPE val, int N)
 
   while (mask <= N)
     {
-      if (mask & N) result *= p;
+      if (mask & N)
+	result *= p;
       p *= p;
       mask <<= 1;
     }
@@ -60,10 +61,10 @@ IEEE_FUNCTION_NAME (DEC_TYPE x, DEC_TYPE y)
   DEC_TYPE y_int;
   DEC_TYPE y_frac;
 
-  int x_class = FUNC_D(__fpclassify) (x);
-  int y_class = FUNC_D(__fpclassify) (y);
+  int x_class = FUNC_D (__fpclassify) (x);
+  int y_class = FUNC_D (__fpclassify) (y);
   if ((x_class == FP_NAN) || (x_class == FP_INFINITE)
-  ||  (y_class == FP_NAN) || (y_class == FP_INFINITE))
+      || (y_class == FP_NAN) || (y_class == FP_INFINITE))
     {
       if ((x_class == FP_NAN) || (y_class == FP_NAN))
 	return DEC_NAN;
@@ -73,18 +74,18 @@ IEEE_FUNCTION_NAME (DEC_TYPE x, DEC_TYPE y)
 	return 1.DD;
       if (y_class == FP_INFINITE && (x_class != FP_NAN))
 	{
-	  DEC_TYPE x_abs = FUNC_D(__fabs) (x);
+	  DEC_TYPE x_abs = FUNC_D (__fabs) (x);
 	  if (x == -1.DD)
 	    return 1.DD;
 	  if (y < 0.DD)
-	    { /* y == -inf */
+	    {			/* y == -inf */
 	      if (x_abs < 1.DD)
 		return DEC_INFINITY;
 	      if (x_abs > 1.DD)
 		return 0.DD;
 	    }
 	  else
-	    { /* y == +inf */
+	    {			/* y == +inf */
 	      if (x_abs < 1.DD)
 		return 0.DD;
 	      if (x_abs > 1.DD)
@@ -93,20 +94,20 @@ IEEE_FUNCTION_NAME (DEC_TYPE x, DEC_TYPE y)
 	}
       if (x_class == FP_INFINITE && (y_class != FP_NAN))
 	{
-	  DEC_TYPE y_int = FUNC_D(__trunc) (y);
+	  DEC_TYPE y_int = FUNC_D (__trunc) (y);
 	  if (y_int == y)
-	    { /* y is an int including 0. */
+	    {			/* y is an int including 0. */
 	      long long int_y = y_int;
-	      int odd_y = (int)(int_y & 1LL);
+	      int odd_y = (int) (int_y & 1LL);
 	      if (odd_y)
-		{ /* y is an odd integer */
-		   if ((y < 0.DD) && (x < 0.DD))
-		     return -0.DD;
-		   if ((y > 0.DD) && (x < 0.DD))
-		     return -DEC_INFINITY;
+		{		/* y is an odd integer */
+		  if ((y < 0.DD) && (x < 0.DD))
+		    return -0.DD;
+		  if ((y > 0.DD) && (x < 0.DD))
+		    return -DEC_INFINITY;
 		}
 	      else
-		{ /* y not an odd integer */
+		{		/* y not an odd integer */
 		  if ((y < 0.DD) && (x < 0.DD))
 		    return 0.DD;
 		  if ((y > 0.DD) && (x < 0.DD))
@@ -116,10 +117,10 @@ IEEE_FUNCTION_NAME (DEC_TYPE x, DEC_TYPE y)
 	  else
 	    {
 	      if (x > 0.DD)
-		{ /* x == +inf */
+		{		/* x == +inf */
 		  if (y < 0.DD)
 		    return 0.DD;
-		  else 
+		  else
 		    return DEC_INFINITY;
 		}
 	    }
@@ -132,53 +133,51 @@ IEEE_FUNCTION_NAME (DEC_TYPE x, DEC_TYPE y)
 
   /* Split y into integer and fraction and use the identity:
      x ^ (m+n) == ( x^m ) * (x^n) */
-  y_int = FUNC_D(__rint) (y);
+  y_int = FUNC_D (__rint) (y);
   y_frac = y - y_int;
 
 #ifdef __DEBUG_PRINT__
-  printf ("y_int=%Df,y_frac=%Df)\n",
-	y_int, y_frac);
+  printf ("y_int=%Df,y_frac=%Df)\n", y_int, y_frac);
 #endif
   if (y_frac == 0.DD)
-    { /* y is an integer */
+    {				/* y is an integer */
       /* FIXME does not handle case where y_int is out of range for
-	 int */
-      int int_y = y_int; /* convert to int */
+         int */
+      int int_y = y_int;	/* convert to int */
       if (y_int > 0.DD)
 	{
 	  int odd_y = int_y & 1;
 	  if (x == 0.0DD)
 	    {
-	      if ( odd_y )
+	      if (odd_y)
 		result = x;
 	      else
 		result = 0.DD;
 	    }
 	  else
 	    {
-	      result = intpow(x, int_y);
+	      result = intpow (x, int_y);
 	    }
 	}
       else
-	{ /* y is negative so use a^-x == 1/(a^x) */
+	{			/* y is negative so use a^-x == 1/(a^x) */
 	  int odd_y = int_y & 1;
 #ifdef __DEBUG_PRINT__
-  printf ("int_y=%d, odd_y=%d)\n",
-	int_y, odd_y);
+	  printf ("int_y=%d, odd_y=%d)\n", int_y, odd_y);
 #endif
 	  if (x == 0.DD)
-	    { /* pole error */
+	    {			/* pole error */
 	      result = HUGE_VAL_D64;
-	      if ( odd_y )
-		result = FUNC_D(__copysign) (result, x);
+	      if (odd_y)
+		result = FUNC_D (__copysign) (result, x);
 	    }
 	  else
-	    result = 1.DD / intpow(x, -int_y);
+	    result = 1.DD / intpow (x, -int_y);
 	}
       /* else y == 0.0, and handled as special case above. */
     }
   else
-    { /* y is not a integer */
+    {				/* y is not a integer */
       if (x == 1.DD)
 	result = 1.DD;
       else if (x == 0.DD)
@@ -186,16 +185,16 @@ IEEE_FUNCTION_NAME (DEC_TYPE x, DEC_TYPE y)
 	  if (y > 0.DD)
 	    result = 0.DD;
 	  else if (y < 0.DD)
-	    result = HUGE_VAL_D64; /* pole error */
+	    result = HUGE_VAL_D64;	/* pole error */
 	  /* else y == 0.0, and handled as special case above. */
 	}
       else
-	{ /* FIXME lots of additional special cases here!  */
-	  int int_y = y_int; /* convert to int */
+	{			/* FIXME lots of additional special cases here!  */
+	  int int_y = y_int;	/* convert to int */
 	  DEC_TYPE log_x;
 
-	  log_x = FUNC_D(__log) (x);
-	  result = intpow(x, int_y) * FUNC_D(__exp) (y_frac * log_x);
+	  log_x = FUNC_D (__log) (x);
+	  result = intpow (x, int_y) * FUNC_D (__exp) (y_frac * log_x);
 	}
     }
 
@@ -207,17 +206,19 @@ INTERNAL_FUNCTION_NAME (DEC_TYPE x, DEC_TYPE y)
 {
   DEC_TYPE z = IEEE_FUNCTION_NAME (x, y);
   /* Pole error: x = 0, y < 0 (non-inf). Set ERANGE in accordance with C99 */
-  if (x == DFP_CONSTANT(0.0) && FUNC_D(__isfinite)(y) && y < DFP_CONSTANT(0.0))
-      DFP_ERRNO (ERANGE);
-  if (!FUNC_D(__isfinite) (z) && FUNC_D(__isfinite) (x) && FUNC_D(__isfinite) (y))
+  if (x == DFP_CONSTANT (0.0) && FUNC_D (__isfinite) (y)
+      && y < DFP_CONSTANT (0.0))
+    DFP_ERRNO (ERANGE);
+  if (!FUNC_D (__isfinite) (z) && FUNC_D (__isfinite) (x)
+      && FUNC_D (__isfinite) (y))
     {
-      if (FUNC_D(__isnan) (z)) /*  Domain error was triggered, x < 0 and y was not an
+      if (FUNC_D (__isnan) (z))	/*  Domain error was triggered, x < 0 and y was not an
 				   odd int */
 	DFP_ERRNO (EDOM);
-      else	/*  Overflow */
+      else			/*  Overflow */
 	DFP_ERRNO (ERANGE);
     }
   return z;
 }
 
-weak_alias(INTERNAL_FUNCTION_NAME, EXTERNAL_FUNCTION_NAME)
+weak_alias (INTERNAL_FUNCTION_NAME, EXTERNAL_FUNCTION_NAME)
