db0bf0ea491a27b98ccb3ef2698995795a9b2393
Paul E. Murphy <murp@ibm.com>
2020-01-03 16:49:21 -0600

    libbid: hacks to support shared library
    PLT bypassing is ugly, and the structure of libbid makes for little
    help.
    
    Inject libdfp-symbols.h and hidden_def/hidden_proto for all GCC
    intrinsics.

--- libbid/bid_gcc_intrinsics.h
+++ libbid/bid_gcc_intrinsics.h
@@ -31,6 +31,8 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 #include "tm.h"
 #include "libgcc_tm.h"
 
+#include "libdfp-symbols.h"
+
 #ifdef __LIBGCC_HAS_XF_MODE__
 #define LIBGCC2_HAS_XF_MODE 1
 #else
@@ -143,108 +145,108 @@ typedef __attribute__ ((aligned(16))) struct
 #if BID_HAS_GCC_DECIMAL_INTRINSICS
 /* Prototypes for gcc instrinsics  */
 
-extern _Decimal64 __bid_adddd3 (_Decimal64, _Decimal64);
-extern _Decimal64 __bid_subdd3 (_Decimal64, _Decimal64);
-extern _Decimal32 __bid_addsd3 (_Decimal32, _Decimal32);
-extern _Decimal32 __bid_subsd3 (_Decimal32, _Decimal32);
-extern _Decimal128 __bid_addtd3 (_Decimal128, _Decimal128);
-extern _Decimal128 __bid_subtd3 (_Decimal128, _Decimal128);
-extern DFtype __bid_truncdddf (_Decimal64);
-extern DItype __bid_fixdddi (_Decimal64);
-extern _Decimal32 __bid_truncddsd2 (_Decimal64);
-extern SFtype __bid_truncddsf (_Decimal64);
-extern SItype __bid_fixddsi (_Decimal64);
-extern _Decimal128 __bid_extendddtd2 (_Decimal64);
+extern _Decimal64 __bid_adddd3 (_Decimal64, _Decimal64); hidden_proto(__bid_adddd3);
+extern _Decimal64 __bid_subdd3 (_Decimal64, _Decimal64); hidden_proto(__bid_subdd3);
+extern _Decimal32 __bid_addsd3 (_Decimal32, _Decimal32); hidden_proto(__bid_addsd3);
+extern _Decimal32 __bid_subsd3 (_Decimal32, _Decimal32); hidden_proto(__bid_subsd3);
+extern _Decimal128 __bid_addtd3 (_Decimal128, _Decimal128); hidden_proto(__bid_addtd3);
+extern _Decimal128 __bid_subtd3 (_Decimal128, _Decimal128); hidden_proto(__bid_subtd3);
+extern DFtype __bid_truncdddf (_Decimal64); hidden_proto(__bid_truncdddf);
+extern DItype __bid_fixdddi (_Decimal64); hidden_proto(__bid_fixdddi);
+extern _Decimal32 __bid_truncddsd2 (_Decimal64); hidden_proto(__bid_truncddsd2);
+extern SFtype __bid_truncddsf (_Decimal64); hidden_proto(__bid_truncddsf);
+extern SItype __bid_fixddsi (_Decimal64); hidden_proto(__bid_fixddsi);
+extern _Decimal128 __bid_extendddtd2 (_Decimal64); hidden_proto(__bid_extendddtd2);
 #if BID_HAS_TF_MODE
-extern TFtype __bid_extendddtf (_Decimal64);
+extern TFtype __bid_extendddtf (_Decimal64); hidden_proto(__bid_extendddtf);
 #endif
-extern UDItype __bid_fixunsdddi (_Decimal64);
-extern USItype __bid_fixunsddsi (_Decimal64);
+extern UDItype __bid_fixunsdddi (_Decimal64); hidden_proto(__bid_fixunsdddi);
+extern USItype __bid_fixunsddsi (_Decimal64); hidden_proto(__bid_fixunsddsi);
 #if BID_HAS_XF_MODE
-extern XFtype __bid_extendddxf (_Decimal64);
+extern XFtype __bid_extendddxf (_Decimal64); hidden_proto(__bid_extendddxf);
 #endif
-extern _Decimal64 __bid_extenddfdd (DFtype);
-extern _Decimal32 __bid_truncdfsd (DFtype);
-extern _Decimal128 __bid_extenddftd (DFtype);
-extern _Decimal64 __bid_floatdidd (DItype);
-extern _Decimal32 __bid_floatdisd (DItype);
-extern _Decimal128 __bid_floatditd (DItype);
-extern _Decimal64 __bid_divdd3 (_Decimal64, _Decimal64);
-extern _Decimal32 __bid_divsd3 (_Decimal32, _Decimal32);
-extern _Decimal128 __bid_divtd3 (_Decimal128, _Decimal128);
-extern CMPtype __bid_eqdd2 (_Decimal64, _Decimal64);
-extern CMPtype __bid_eqsd2 (_Decimal32, _Decimal32);
-extern CMPtype __bid_eqtd2 (_Decimal128, _Decimal128);
-extern CMPtype __bid_gedd2 (_Decimal64, _Decimal64);
-extern CMPtype __bid_gesd2 (_Decimal32, _Decimal32);
-extern CMPtype __bid_getd2 (_Decimal128, _Decimal128);
-extern CMPtype __bid_gtdd2 (_Decimal64, _Decimal64);
-extern CMPtype __bid_gtsd2 (_Decimal32, _Decimal32);
-extern CMPtype __bid_gttd2 (_Decimal128, _Decimal128);
-extern CMPtype __bid_ledd2 (_Decimal64, _Decimal64);
-extern CMPtype __bid_lesd2 (_Decimal32, _Decimal32);
-extern CMPtype __bid_letd2 (_Decimal128, _Decimal128);
-extern CMPtype __bid_ltdd2 (_Decimal64, _Decimal64);
-extern CMPtype __bid_ltsd2 (_Decimal32, _Decimal32);
-extern CMPtype __bid_lttd2 (_Decimal128, _Decimal128);
-extern CMPtype __bid_nedd2 (_Decimal64, _Decimal64);
-extern CMPtype __bid_nesd2 (_Decimal32, _Decimal32);
-extern CMPtype __bid_netd2 (_Decimal128, _Decimal128);
-extern CMPtype __bid_unorddd2 (_Decimal64, _Decimal64);
-extern CMPtype __bid_unordsd2 (_Decimal32, _Decimal32);
-extern CMPtype __bid_unordtd2 (_Decimal128, _Decimal128);
-extern _Decimal64 __bid_muldd3 (_Decimal64, _Decimal64);
-extern _Decimal32 __bid_mulsd3 (_Decimal32, _Decimal32);
-extern _Decimal128 __bid_multd3 (_Decimal128, _Decimal128);
-extern _Decimal64 __bid_extendsddd2 (_Decimal32);
-extern DFtype __bid_extendsddf (_Decimal32);
-extern DItype __bid_fixsddi (_Decimal32);
-extern SFtype __bid_truncsdsf (_Decimal32);
-extern SItype __bid_fixsdsi (_Decimal32);
-extern _Decimal128 __bid_extendsdtd2 (_Decimal32);
+extern _Decimal64 __bid_extenddfdd (DFtype); hidden_proto(__bid_extenddfdd);
+extern _Decimal32 __bid_truncdfsd (DFtype); hidden_proto(__bid_truncdfsd);
+extern _Decimal128 __bid_extenddftd (DFtype); hidden_proto(__bid_extenddftd);
+extern _Decimal64 __bid_floatdidd (DItype); hidden_proto(__bid_floatdidd);
+extern _Decimal32 __bid_floatdisd (DItype); hidden_proto(__bid_floatdisd);
+extern _Decimal128 __bid_floatditd (DItype); hidden_proto(__bid_floatditd);
+extern _Decimal64 __bid_divdd3 (_Decimal64, _Decimal64); hidden_proto(__bid_divdd3);
+extern _Decimal32 __bid_divsd3 (_Decimal32, _Decimal32); hidden_proto(__bid_divsd3);
+extern _Decimal128 __bid_divtd3 (_Decimal128, _Decimal128); hidden_proto(__bid_divtd3);
+extern CMPtype __bid_eqdd2 (_Decimal64, _Decimal64); hidden_proto(__bid_eqdd2);
+extern CMPtype __bid_eqsd2 (_Decimal32, _Decimal32); hidden_proto(__bid_eqsd2);
+extern CMPtype __bid_eqtd2 (_Decimal128, _Decimal128); hidden_proto(__bid_eqtd2);
+extern CMPtype __bid_gedd2 (_Decimal64, _Decimal64); hidden_proto(__bid_gedd2);
+extern CMPtype __bid_gesd2 (_Decimal32, _Decimal32); hidden_proto(__bid_gesd2);
+extern CMPtype __bid_getd2 (_Decimal128, _Decimal128); hidden_proto(__bid_getd2);
+extern CMPtype __bid_gtdd2 (_Decimal64, _Decimal64); hidden_proto(__bid_gtdd2);
+extern CMPtype __bid_gtsd2 (_Decimal32, _Decimal32); hidden_proto(__bid_gtsd2);
+extern CMPtype __bid_gttd2 (_Decimal128, _Decimal128); hidden_proto(__bid_gttd2);
+extern CMPtype __bid_ledd2 (_Decimal64, _Decimal64); hidden_proto(__bid_ledd2);
+extern CMPtype __bid_lesd2 (_Decimal32, _Decimal32); hidden_proto(__bid_lesd2);
+extern CMPtype __bid_letd2 (_Decimal128, _Decimal128); hidden_proto(__bid_letd2);
+extern CMPtype __bid_ltdd2 (_Decimal64, _Decimal64); hidden_proto(__bid_ltdd2);
+extern CMPtype __bid_ltsd2 (_Decimal32, _Decimal32); hidden_proto(__bid_ltsd2);
+extern CMPtype __bid_lttd2 (_Decimal128, _Decimal128); hidden_proto(__bid_lttd2);
+extern CMPtype __bid_nedd2 (_Decimal64, _Decimal64); hidden_proto(__bid_nedd2);
+extern CMPtype __bid_nesd2 (_Decimal32, _Decimal32); hidden_proto(__bid_nesd2);
+extern CMPtype __bid_netd2 (_Decimal128, _Decimal128); hidden_proto(__bid_netd2);
+extern CMPtype __bid_unorddd2 (_Decimal64, _Decimal64); hidden_proto(__bid_unorddd2);
+extern CMPtype __bid_unordsd2 (_Decimal32, _Decimal32); hidden_proto(__bid_unordsd2);
+extern CMPtype __bid_unordtd2 (_Decimal128, _Decimal128); hidden_proto(__bid_unordtd2);
+extern _Decimal64 __bid_muldd3 (_Decimal64, _Decimal64); hidden_proto(__bid_muldd3);
+extern _Decimal32 __bid_mulsd3 (_Decimal32, _Decimal32); hidden_proto(__bid_mulsd3);
+extern _Decimal128 __bid_multd3 (_Decimal128, _Decimal128); hidden_proto(__bid_multd3);
+extern _Decimal64 __bid_extendsddd2 (_Decimal32); hidden_proto(__bid_extendsddd2);
+extern DFtype __bid_extendsddf (_Decimal32); hidden_proto(__bid_extendsddf);
+extern DItype __bid_fixsddi (_Decimal32); hidden_proto(__bid_fixsddi);
+extern SFtype __bid_truncsdsf (_Decimal32); hidden_proto(__bid_truncsdsf);
+extern SItype __bid_fixsdsi (_Decimal32); hidden_proto(__bid_fixsdsi);
+extern _Decimal128 __bid_extendsdtd2 (_Decimal32); hidden_proto(__bid_extendsdtd2);
 #if BID_HAS_TF_MODE
-extern TFtype __bid_extendsdtf (_Decimal32);
+extern TFtype __bid_extendsdtf (_Decimal32); hidden_proto(__bid_extendsdtf);
 #endif
-extern UDItype __bid_fixunssddi (_Decimal32);
-extern USItype __bid_fixunssdsi (_Decimal32);
+extern UDItype __bid_fixunssddi (_Decimal32); hidden_proto(__bid_fixunssddi);
+extern USItype __bid_fixunssdsi (_Decimal32); hidden_proto(__bid_fixunssdsi);
 #if BID_HAS_XF_MODE
-extern XFtype __bid_extendsdxf (_Decimal32);
+extern XFtype __bid_extendsdxf (_Decimal32); hidden_proto(__bid_extendsdxf);
 #endif
-extern _Decimal64 __bid_extendsfdd (SFtype);
-extern _Decimal32 __bid_extendsfsd (SFtype);
-extern _Decimal128 __bid_extendsftd (SFtype);
-extern _Decimal64 __bid_floatsidd (SItype);
-extern _Decimal32 __bid_floatsisd (SItype);
-extern _Decimal128 __bid_floatsitd (SItype);
-extern _Decimal64 __bid_trunctddd2 (_Decimal128);
-extern DFtype __bid_trunctddf (_Decimal128);
-extern DItype __bid_fixtddi (_Decimal128);
-extern _Decimal32 __bid_trunctdsd2 (_Decimal128);
-extern SFtype __bid_trunctdsf (_Decimal128);
-extern SItype __bid_fixtdsi (_Decimal128);
+extern _Decimal64 __bid_extendsfdd (SFtype); hidden_proto(__bid_extendsfdd);
+extern _Decimal32 __bid_extendsfsd (SFtype); hidden_proto(__bid_extendsfsd);
+extern _Decimal128 __bid_extendsftd (SFtype); hidden_proto(__bid_extendsftd);
+extern _Decimal64 __bid_floatsidd (SItype); hidden_proto(__bid_floatsidd);
+extern _Decimal32 __bid_floatsisd (SItype); hidden_proto(__bid_floatsisd);
+extern _Decimal128 __bid_floatsitd (SItype); hidden_proto(__bid_floatsitd);
+extern _Decimal64 __bid_trunctddd2 (_Decimal128); hidden_proto(__bid_trunctddd2);
+extern DFtype __bid_trunctddf (_Decimal128); hidden_proto(__bid_trunctddf);
+extern DItype __bid_fixtddi (_Decimal128); hidden_proto(__bid_fixtddi);
+extern _Decimal32 __bid_trunctdsd2 (_Decimal128); hidden_proto(__bid_trunctdsd2);
+extern SFtype __bid_trunctdsf (_Decimal128); hidden_proto(__bid_trunctdsf);
+extern SItype __bid_fixtdsi (_Decimal128); hidden_proto(__bid_fixtdsi);
 #if BID_HAS_TF_MODE
-extern TFtype __bid_trunctdtf (_Decimal128);
+extern TFtype __bid_trunctdtf (_Decimal128); hidden_proto(__bid_trunctdtf);
 #endif
-extern UDItype __bid_fixunstddi (_Decimal128);
-extern USItype __bid_fixunstdsi (_Decimal128);
+extern UDItype __bid_fixunstddi (_Decimal128); hidden_proto(__bid_fixunstddi);
+extern USItype __bid_fixunstdsi (_Decimal128); hidden_proto(__bid_fixunstdsi);
 #if BID_HAS_XF_MODE
-extern XFtype __bid_trunctdxf (_Decimal128);
+extern XFtype __bid_trunctdxf (_Decimal128); hidden_proto(__bid_trunctdxf);
 #endif
 #if BID_HAS_TF_MODE
-extern _Decimal64 __bid_trunctfdd (TFtype);
-extern _Decimal32 __bid_trunctfsd (TFtype);
-extern _Decimal128 __bid_extendtftd (TFtype);
+extern _Decimal64 __bid_trunctfdd (TFtype); hidden_proto(__bid_trunctfdd);
+extern _Decimal32 __bid_trunctfsd (TFtype); hidden_proto(__bid_trunctfsd);
+extern _Decimal128 __bid_extendtftd (TFtype); hidden_proto(__bid_extendtftd);
 #endif
-extern _Decimal64 __bid_floatunsdidd (UDItype);
-extern _Decimal32 __bid_floatunsdisd (UDItype);
-extern _Decimal128 __bid_floatunsditd (UDItype);
-extern _Decimal64 __bid_floatunssidd (USItype);
-extern _Decimal32 __bid_floatunssisd (USItype);
-extern _Decimal128 __bid_floatunssitd (USItype);
+extern _Decimal64 __bid_floatunsdidd (UDItype); hidden_proto(__bid_floatunsdidd);
+extern _Decimal32 __bid_floatunsdisd (UDItype); hidden_proto(__bid_floatunsdisd);
+extern _Decimal128 __bid_floatunsditd (UDItype); hidden_proto(__bid_floatunsditd);
+extern _Decimal64 __bid_floatunssidd (USItype); hidden_proto(__bid_floatunssidd);
+extern _Decimal32 __bid_floatunssisd (USItype); hidden_proto(__bid_floatunssisd);
+extern _Decimal128 __bid_floatunssitd (USItype); hidden_proto(__bid_floatunssitd);
 #if BID_HAS_XF_MODE
-extern _Decimal64 __bid_truncxfdd (XFtype);
-extern _Decimal32 __bid_truncxfsd (XFtype);
-extern _Decimal128 __bid_extendxftd (XFtype);
+extern _Decimal64 __bid_truncxfdd (XFtype); hidden_proto(__bid_truncxfdd);
+extern _Decimal32 __bid_truncxfsd (XFtype); hidden_proto(__bid_truncxfsd);
+extern _Decimal128 __bid_extendxftd (XFtype); hidden_proto(__bid_extendxftd);
 #endif
 extern int isinfd32 (_Decimal32);
 extern int isinfd64 (_Decimal64);
