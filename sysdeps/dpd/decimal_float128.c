474cacbfb3d84794fe2535aed85a7e6d83cc663a
Paul E. Murphy <murphyp@linux.vnet.ibm.com>
2020-02-07 17:34:51 -0600

    Add float128 support
    This adds string conversions between _Float128 and _DecimalN for
    ppc64le.  x86 gets this for free via libbid routines which had
    not previously been exported in the shared library.
    
    Notably, this brings in the first divergence between ppc64 and
    ppc64le.  Thus, a new sysdep dir is added to handle ppc64le and
    it's new ABI.  Similarly, autotools does a bad job detecting a
    ppc64le host.  We workaround by appending le if we detect little
    endian.  Similarly, no ppc64le exist without hardware dfp.  So,
    keep it simple with a single abilist file.
    
    Similarly, this support bumps the toolchain requirements for
    ppc64le and x86 platforms.  This requires GCC 6.2 for these
    targets, and glibc 2.26.
    
    Note, s390 is unchanged.  It supports already supports the float128
    format as its long double type.
    
    A future patch will add explicit testing for KFmode on ppc64le.
    This requires a rewrite of the testsuite to support all the
    various GCC mode combinations.  Likewise, the rewrite hinges on
    the header cleanup which also enables C11 internally.

---
+++ sysdeps/dpd/decimal_float128.c
@@ -0,0 +1,79 @@
+/* Conversions between _Float128 and _DecimalN types
+
+   Copyright (C) 2020 Free Software Foundation, Inc.
+
+   This file is part of the Decimal Floating Point C Library.
+
+   The Decimal Floating Point C Library is free software; you can
+   redistribute it and/or modify it under the terms of the GNU Lesser
+   General Public License version 2.1.
+
+   The Decimal Floating Point C Library is distributed in the hope that
+   it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+   the GNU Lesser General Public License version 2.1 for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License version 2.1 along with the Decimal Floating Point C Library;
+   if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+   Suite 330, Boston, MA 02111-1307 USA.
+
+   Please see libdfp/COPYING.txt for more information.  */
+
+#define _GNU_SOURCE
+#define __STDC_WANT_IEC_60559_TYPES_EXT__
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Order matters to ensure decNumber object is sufficiently large. */
+#include <decimal128.h>
+#include <decimal64.h>
+#include <decimal32.h>
+#include <convert.h>
+
+#define BUFMAX 128
+
+#define DECL_TO_F128(fmode,dmode,N,func) \
+    _Float128 \
+    func ## dmode ## fmode ## 2 (_Decimal ## N a); \
+    _Float128 \
+    func ## dmode ## fmode ## 2 (_Decimal ## N a) \
+    { \
+      char buf[BUFMAX]; \
+      decNumber n; \
+      decContext context; \
+      decContextDefault (&context, DEC_INIT_DECIMAL ## N); \
+      decimal ## N ## ToNumber ((decimal ## N *)&a, &n); \
+      decNumberToString (&n, buf); \
+      return strtof128 (buf, NULL); \
+    }
+
+DECL_TO_F128 (kf, sd, 32, __dpd_extend);
+DECL_TO_F128 (kf, dd, 64, __dpd_extend);
+DECL_TO_F128 (td, kf, 128, __dpd_trunc);
+
+#define DECL_FROM_F128(fmode,dmode,N,func) \
+    _Decimal ## N \
+    func ## fmode ## dmode ## 2 (_Float128 a); \
+    _Decimal ## N \
+    func ## fmode ## dmode ## 2 (_Float128 a) \
+    { \
+       _Decimal ## N f;\
+       char buf[BUFMAX]; \
+       decContext context; \
+       strfromf128 (buf, BUFMAX, "%.35e", a); \
+       decContextDefault (&context, DEC_INIT_DECIMAL ## N); \
+       decimal ## N ## FromString( (decimal ## N *) &f, buf, &context); \
+       if (context.status != 0) { \
+         int dec_flags = context.status & \
+           (DEC_IEEE_854_Inexact|DEC_IEEE_854_Invalid_operation|DEC_IEEE_854_Overflow); \
+           DFP_HANDLE_EXCEPTIONS(DFP_IEEE_FLAGS(dec_flags)); \
+       } \
+       return f; \
+    }
+
+DECL_FROM_F128 (kf, sd, 32, __dpd_trunc);
+DECL_FROM_F128 (kf, dd, 64, __dpd_trunc);
+DECL_FROM_F128 (td, kf, 128, __dpd_extend);
