348d3d6b9fc21016c46d2dbd4f9a137ea51920eb
Paul E. Murphy <murp@ibm.com>
2024-02-07 14:06:00 -0600

    ieee754r: add missing table entry to logd128
    Depending on the rounding mode, the lookup table needs one
    more entry to avoid an overflow for values like 0.9995e10
    which can round up to 1.0.
    
    Likewise, print out the copyright header in declntables.c
    to simplify table regeneration.
    
    fixes #199

--- ieee754r/declntables.c
+++ ieee754r/declntables.c
@@ -23,15 +23,48 @@
    Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
    Please see libdfp/COPYING.txt for more information.
+
+   gcc declntables.c -O2 -ldecnumber -o declntables
+   ./declntables > t_logd128.h
 */
 
 #define DECNUMDIGITS 68
 
-#include "decNumber.h"
+#include <decnumber/decContext.h>
+#include <decnumber/decNumber.h>
 #include <string.h>
 #include <stdlib.h>
 
 const decNumber *dzero;
+
+const char COPYRIGHT[] =
+"/* Generated table of ln(x) - (t - 1) table values.\n"
+"\n"
+"   See declntables.c to understand how, or fix these\n"
+"   values if such is neeeded.\n"
+"\n"
+"   Copyright (C) 2019 Free Software Foundation, Inc.\n"
+"\n"
+"   This file is part of the Decimal Floating Point C Library.\n"
+"\n"
+"   This is adapted, derived, and in some spiritual form based on\n"
+"   the version found in glibc's float128/ibm128 variant.\n"
+"\n"
+"   The Decimal Floating Point C Library is free software; you can\n"
+"   redistribute it and/or modify it under the terms of the GNU Lesser\n"
+"   General Public License version 2.1.\n"
+"\n"
+"   The Decimal Floating Point C Library is distributed in the hope that\n"
+"   it will be useful, but WITHOUT ANY WARRANTY; without even the implied\n"
+"   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See\n"
+"   the GNU Lesser General Public License version 2.1 for more details.\n"
+"\n"
+"   You should have received a copy of the GNU Lesser General Public\n"
+"   License version 2.1 along with the Decimal Floating Point C Library;\n"
+"   if not, write to the Free Software Foundation, Inc., 51 Franklin\n"
+"   Street, Fifth Floor, Boston, MA 02110-1301 USA.\n"
+"\n"
+"   Please see libdfp/COPYING.txt for more information.  */\n";
 
 static char TBLHDRSTR[] =
   "static const struct { _Decimal128 t; _Decimal128 lnt; } %s[] = {\n";
@@ -78,6 +111,7 @@ main (void)
   decNumberFromInt32 (&_1, 1);
   decNumberFromInt32 (&_1000, decade_shift);
 
+  puts (COPYRIGHT);
   printf (TBLHDRSTR, "lntvals");
   printf ("  {%5sDL, %+36sDL},\n", "1.0", "0.0");
   for (int ix = decade_shift + prec / 10; ix < ixe; ix += 10)
@@ -88,12 +122,10 @@ main (void)
     {
       p_lntptm1 (ix, &_1, &ctx, &_1000);
     }
+  /* And one final entry if the rounded value rounds up to 1.0. */
+  printf ("  {%5sDL, %+36sDL},\n", "1.0", "0.0");
   printf ("};\n");
   printf ("#define LNTVALS_START %d\n", prec);
 
   return 0;
 };
-
-/* This is kind of slow, but it simplifies life greatly. */
-#include "decNumber.c"
-#include "decContext.c"
